76c716823921d8167797bc5b909c2267afe7ba1c
Simon Pintado <simon.pintado@mxglobalsolutions.com>
2018-03-13 13:42:18 -0700

    Agrego formulario a crear tarea y separo con for datos de array de objetos

--- src/pages/creartarea/creartarea.ts
+++ src/pages/creartarea/creartarea.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {NuevaTareaProvider} from '../../providers/nueva-tarea/nueva-tarea';
-/**
- * Generated class for the CreartareaPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+
+//importo formBuilder
+import {FormBuilder,FormGroup,Validator, Validators} from '@angular/forms';
 
 @IonicPage()
 @Component({
@@ -14,21 +11,44 @@ import {NuevaTareaProvider} from '../../providers/nueva-tarea/nueva-tarea';
   templateUrl: 'creartarea.html',
 })
 export class CreartareaPage {
+//bandera
+flag=true;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private _nt:NuevaTareaProvider) {
+
+
+//declaro variables que usare para validar formulario
+myForm: FormGroup;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+  public fb:FormBuilder) {
+
+    this.myForm = this.fb.group({
+      name: ['', [Validators.required]],
+      company: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(10)]],
+      email: ['', [Validators.required, Validators.email]],
+      task:['',[Validators.required]]
+      /*
+      age: ['', [Validators.required]],
+      url: ['', [Validators.pattern(/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/)]],
+      password: ['', [Validators.pattern(/^[a-z0-9_-]{6,18}$/)]],
+      */
+    })
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad CreartareaPage');
+  saveData(){
+    alert(JSON.stringify(this.myForm.value));
+    console.log("Formato JSON");
+    console.log(JSON.stringify(this.myForm.value));
+    console.log("SIN JSON");
+    console.log(this.myForm.value);
   }
 
-  //Definicion de arreglo vacio que recibira los datos del form
-  todo={}
+  showTask(){
+    return this.flag=false;
 
-  //Impresion de datos
-  logForm(){
-    console.log(this.todo);
   }
+
+
 
 
 
--- src/pages/mostrartarea/mostrartarea.ts
+++ src/pages/mostrartarea/mostrartarea.ts
@@ -8,29 +8,71 @@ import { ObtenerTareaProvider} from '../../providers/obtener-tarea/obtener-tarea
   templateUrl: 'mostrartarea.html',
 })
 export class MostrartareaPage {
+
   users:any[]=[];
+
+  //componentes necesarios para realizar busqueda filtrada
+  searchQuery: string = '';
+  items: string[];
+
   constructor(public navCtrl: NavController, public navParams: NavParams,public _ts:ObtenerTareaProvider) {
+    this.getUsersWeb();
 
   }
 
 
+
   ionViewDidLoad() {
     console.log('ionViewDidLoad MostrartareaPage');
-    //this._ts.mostrar_tareas();
 
+  }
+
+  getUsersWeb(){
     this._ts.getUsers()
       .subscribe(
         (data)=>{
-          console.log(data);
-          /*
-          this.users=data['task'];
-          console.log(this.users=data['task']);
-          */
+          this.users=data['results'];
+          console.log(this.users=data['results']);
+          //var result=JSON.stringify(this.users);
+          console.log(this.users);
+
+
+          for(var i=0;i<this.users.length;i++){
+            console.log("Recorriendo con for");
+            console.log(this.users[i]['name']['first']);
+            this.items=this.users[i]['name']['first'];
+          }
+
         },
         (error)=>{
           console.log(error);
         }
       )
+  }
+
+fillArray(items){
+
+}
+
+/*
+initializeItems() {
+    this.items = [
+      'Amsterdam',
+      'Bogota'
+    ];
+  }
+*/
+
+  getItems(ev: any){
+    //asigno el valor a searchbar
+    let val=ev.target.value;
+
+    //si el valor es un string vacio no filtra items
+    if (val && val.trim() != '') {
+      this.items = this.items.filter((item) => {
+        return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
   }
 
 
